027931c8ba87d9a4a33de50fa6b31d2499cc4a16
Minsu Lee <minsulee0326@gmail.com>
2019-02-28 16:12:28 +0900

    Update 2_deploy_contracts for Study

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+// var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var Study = artifacts.require("./Study.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  // deployer.deploy(SimpleStorage);
+  deployer.deploy(Study)
 };
