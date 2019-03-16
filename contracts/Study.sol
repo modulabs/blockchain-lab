34b5ca6ea3faf90b4a1be76000880c311d0903c1
currybab <pj2417@gmail.com>
2019-03-16 16:04:39 +0900

    solhint configuration added

--- contracts/Study.sol
+++ contracts/Study.sol
@@ -20,7 +20,7 @@ contract Study {
   constructor(bytes32 className, bytes32[] memory lectureNames) public {
     labAdmin = msg.sender;
     labName = className;
-    for(uint i=0; i<lectureNames.length; i++) {
+    for (uint i=0; i < lectureNames.length; i++) {
       class.push(Lecture({
         lectureName: lectureNames[i],
         attandee: new address[](0)
@@ -28,7 +28,7 @@ contract Study {
     }
   }
 
-  function kill() public{
+  function kill() public {
     require(msg.sender == labAdmin);
     selfdestruct(msg.sender);
   }
@@ -37,26 +37,27 @@ contract Study {
     return labAdmin;
   }
 
-  function setStudent(bytes32 name, bytes32 email) public{
+  function setStudent(bytes32 name, bytes32 email) public {
     students[msg.sender] = Student(name, email);
   }
 
-  function getStudent(address st) public view returns(bytes32, bytes32) {
+  function getStudent(address st) public view returns (bytes32, bytes32) {
     require(msg.sender == st || msg.sender == labAdmin);
     return (students[st].name, students[st].email);
   }
 
   function getLectures() public view returns (bytes32[] memory lectureList) {
     lectureList = new bytes32[](class.length);
-    for(uint i=0; i<class.length; i++) {
+    for (uint i=0; i < class.length; i++) {
       lectureList[i] = class[i].lectureName;
     }
   }
+
   function setAttendance(uint lectureNum) public {
     class[lectureNum].attandee.push(msg.sender);
   }
 
-  function getAttendance(uint lectureNum) public view returns (address[] memory){
+  function getAttendance(uint lectureNum) public view returns (address[] memory) {
     return class[lectureNum].attandee;
   }
 }
