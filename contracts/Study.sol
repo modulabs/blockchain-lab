e93cf4e857ec478c9d263182c6eee38e71abd8f7
currybab <pj2417@gmail.com>
2019-03-18 16:20:20 +0900

    Lecture 상위에 Lab을 만들고 테스트 코드 일부 수정

--- contracts/Study.sol
+++ contracts/Study.sol
@@ -7,34 +7,78 @@ contract Study {
     bytes32 email;
   }
 
-  struct Lecture {
-    bytes32 lectureName;
-    address[] attandee;
+  struct Admin {
+    bytes32 name;
+    bytes32 email;
   }
 
-  address labAdmin;
-  bytes32 labName;
-  mapping(address => Student) public students;
-  Lecture[] class;
+  struct Lecture {
+    bytes32 lectureName;
+    mapping(uint => address) attendee;
+    uint attendeeSize;
+  }
 
-  constructor(bytes32 className, bytes32[] memory lectureNames) public {
-    labAdmin = msg.sender;
-    labName = className;
-    for (uint i=0; i < lectureNames.length; i++) {
-      class.push(Lecture({
+  struct Lab {
+    address adminAddr;
+    mapping(uint => Lecture) lectures;
+    uint lecturesSize;
+  }
+  
+  modifier onlyLabAdmin(bytes32 labName) {
+    require(msg.sender == labs[labName].adminAddr, "Only Lab Admin can call this function!");
+    _;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == contractOwner, "Only contract owner can call this function!");
+    _;
+  }
+
+  mapping(address => Student) public students;
+  mapping(address => Admin) public admins;
+  mapping(bytes32 => Lab) public labs;
+  bytes32[] labNames;
+
+  address payable contractOwner;
+
+  constructor() public {
+    contractOwner = msg.sender;
+  }
+
+  function setAdmin(bytes32 name, bytes32 email) public {
+    admins[msg.sender] = Admin(name, email);
+  }
+
+  function getAdmin(address addr) public view returns(bytes32, bytes32) {
+    Admin memory admin = admins[addr];
+    return (admin.name, admin.email);
+  }
+
+  function getLabAdmin(bytes32 labName) public view returns(bytes32, bytes32) {
+    address addr = labs[labName].adminAddr;
+    Admin memory admin = admins[addr];
+    return (admin.name, admin.email);
+  }
+
+  function createLab(bytes32 labName) public {
+    labs[labName] = Lab({
+      adminAddr: msg.sender,
+      lecturesSize: 0
+    });
+    labNames.push(labName);
+  }
+
+  function setLab(bytes32 labName, bytes32[] memory lectureNames) public {
+    for (uint i = 0; i < lectureNames.length; i++) {
+      labs[labName].lectures[i] = Lecture({
         lectureName: lectureNames[i],
-        attandee: new address[](0)
-      }));
+        attendeeSize: 0
+      });
     }
   }
 
-  function kill() public {
-    require(msg.sender == labAdmin);
-    selfdestruct(msg.sender);
-  }
-
-  function getAdmin() public view returns(address) {
-    return labAdmin;
+  function kill() public onlyOwner {
+    selfdestruct(contractOwner);
   }
 
   function setStudent(bytes32 name, bytes32 email) public {
@@ -42,22 +86,26 @@ contract Study {
   }
 
   function getStudent(address st) public view returns (bytes32, bytes32) {
-    require(msg.sender == st || msg.sender == labAdmin);
+    require(msg.sender == st || msg.sender == contractOwner);
     return (students[st].name, students[st].email);
   }
 
-  function getLectures() public view returns (bytes32[] memory lectureList) {
-    lectureList = new bytes32[](class.length);
-    for (uint i=0; i < class.length; i++) {
-      lectureList[i] = class[i].lectureName;
+  function getLectures(bytes32 labName) public view returns (bytes32[] memory lectureList) {
+    lectureList = new bytes32[](labs[labName].lecturesSize);
+    for (uint i = 0; i < labs[labName].lecturesSize; i++) {
+      lectureList[i] = labs[labName].lectures[i].lectureName;
     }
   }
 
-  function setAttendance(uint lectureNum) public {
-    class[lectureNum].attandee.push(msg.sender);
+  function setAttendance(bytes32 labName, uint lectureNum) public {
+    labs[labName].lectures[lectureNum].attendee[labs[labName].lectures[lectureNum].attendeeSize] = msg.sender;
+    labs[labName].lectures[lectureNum].attendeeSize += 1;
   }
 
-  function getAttendance(uint lectureNum) public view returns (address[] memory) {
-    return class[lectureNum].attandee;
+  function getAttendance(bytes32 labName, uint lectureNum) public view returns (address[] memory attendeeList) {
+    attendeeList = new address[](labs[labName].lectures[lectureNum].attendeeSize);
+    for (uint i = 0; i < labs[labName].lectures[lectureNum].attendeeSize; i++) {
+      attendeeList[i] = labs[labName].lectures[lectureNum].attendee[i];
+    }
   }
 }
--- test/study.js
+++ test/study.js
@@ -5,11 +5,11 @@ contract("Study", accounts => {
     const StudyInstance = await Study.deployed();
 
     // Set value of Admin
-    await StudyInstance.setAdmin("Admin", { from: accounts[0] });
+    await StudyInstance.setAdmin(web3.utils.fromAscii("Admin"), web3.utils.fromAscii("admin@modulabs.co.kr"), { from: accounts[0] });
 
     // Get stored value
-    const storedData = await StudyInstance.getAdmin.call();
-
-    assert.equal(storedData, "Admin", "The value Admin was not stored.");
+    const storedData = await StudyInstance.getAdmin.call(accounts[0]);
+    assert.equal(web3.utils.toAscii(storedData[0]).replace(/\0/g, ''), "Admin", "The value name was not stored.");
+    assert.equal(web3.utils.toAscii(storedData[1]).replace(/\0/g, ''), "admin@modulabs.co.kr", "The value email was not stored.");
   });
 });
