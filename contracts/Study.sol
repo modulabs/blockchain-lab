363b37afee2e4042aa16c5c4433d2b791e614ef9
Jun-Sik Choi <leichoi91@gmail.com>
2019-03-20 21:18:10 +0900

    Separating Lab cont. and Study cont.

--- contracts/Study.sol
+++ contracts/Study.sol
@@ -1,30 +1,5 @@
 pragma solidity ^0.5.0;
 
-contract Lab {
-    address public _labOwner;
-    string public labName;
-    address[] public studyList;
-
-    event StudyCreated(address studyAddr, address studyAdmin);
-
-    constructor () public {
-        _labOwner = msg.sender;
-    }
-
-    function setLabName (string memory _labName) public {
-        require(msg.sender == _labOwner);
-        labName = _labName;
-    }
-
-    function createStudy(string memory _studyName) public {
-        Study newStudy = new Study();
-        newStudy.setStudyName(_studyName);
-        address _addr = address(newStudy);
-        studyList.push(_addr);
-        emit StudyCreated(_addr, msg.sender);
-    }
-}
-
 contract Study {
 
     struct Student {
