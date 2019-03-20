5a1030a1ab330589d0f74edc8715c2afd4ffdccb
Jun-Sik Choi <leichoi91@gmail.com>
2019-03-20 19:11:15 +0900

    Add Lab contract that is able to create and save multiple Study contracts

--- contracts/Study.sol
+++ contracts/Study.sol
@@ -1,33 +1,95 @@
 pragma solidity ^0.5.0;
 
+contract Lab {
+    address public _labOwner;
+    string public labName;
+    address[] public studyList;
+
+    event StudyCreated(address studyAddr, address studyAdmin);
+
+    constructor () public {
+        _labOwner = msg.sender;
+    }
+
+    function setLabName (string memory _labName) public {
+        require(msg.sender == _labOwner);
+        labName = _labName;
+    }
+
+    function createStudy(string memory _studyName) public {
+        Study newStudy = new Study();
+        newStudy.setStudyName(_studyName);
+        address _addr = address(newStudy);
+        studyList.push(_addr);
+        emit StudyCreated(_addr, msg.sender);
+    }
+}
+
 contract Study {
-  string labAdmin;
-  uint numOfStudent = 2; // hardcoded for now
-  bytes8[] memberList = new bytes8[](numOfStudent); // init list
-  uint numOfSylabus = 5;
-  bytes4[] syllabusList = new bytes4[](numOfSylabus);
 
-  function setAdmin(string memory s) public {
-    labAdmin = s;
-  }
+    struct Student {
+        bytes32 name;
+        bytes32 email;
+    }
 
-  function getAdmin() public view returns(string memory) {
-    return labAdmin;
-  }
+    struct Lecture {
+        bytes32 lectureName;
+        address[] attandee;
+    }
 
-  function setMember(bytes8 member) public {
-    memberList.push(member);
-  }
+    address studyAdmin;
+    string studyName;
+    mapping(address => Student) public students;
+    Lecture[] class;
 
-  function getMembers() public returns(bytes8[] memory) {
-    return memberList;
-  }
+    constructor() public {
+        studyAdmin = msg.sender;
 
-  function setSyllabus(bytes4 sylabus) public {
-    syllabusList.push(sylabus);
-  }
+    }
 
-  function getSyllabus() public returns(bytes4[] memory) {
-    return syllabusList;
-  }
+    function setLecture(bytes32[] memory lectureNames) public {
+        require(msg.sender == studyAdmin);
+        for(uint i=0; i<lectureNames.length; i++) {
+            class.push(Lecture({
+                lectureName: lectureNames[i],
+                attandee: new address[](0)
+            }));
+        }
+    }
+
+    function setStudyName(string memory _studyName) public {
+        studyName = _studyName;
+    }
+
+    function kill() public{
+        require(msg.sender == studyAdmin);
+        selfdestruct(msg.sender);
+    }
+
+    function getAdmin() public view returns(address) {
+        return studyAdmin;
+    }
+
+    function setStudent(bytes32 name, bytes32 email) public{
+        students[msg.sender] = Student(name, email);
+    }
+
+    function getStudent(address st) public view returns(bytes32, bytes32) {
+        require(msg.sender == st || msg.sender == studyAdmin);
+        return (students[st].name, students[st].email);
+    }
+
+    function getLectures() public view returns (bytes32[] memory lectureList) {
+        lectureList = new bytes32[](class.length);
+        for(uint i=0; i<class.length; i++) {
+            lectureList[i] = class[i].lectureName;
+        }
+    }
+    function setAttendance(uint lectureNum) public {
+        class[lectureNum].attandee.push(msg.sender);
+    }
+
+    function getAttendance(uint lectureNum) public view returns (address[] memory){
+        return class[lectureNum].attandee;
+    }
 }
