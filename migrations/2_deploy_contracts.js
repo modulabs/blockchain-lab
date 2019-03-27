51c7842f9d97426df5e976424bef4063db40855d
aristAdmin <git@arist.io>
2019-03-27 20:52:06 +0900

    Not deploy Study but Lab

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,7 @@
-// var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-var Study = artifacts.require("./Study.sol");
+var Lab = artifacts.require("./Lab.sol")
 
 module.exports = function(deployer) {
   // Class defined with Lectures when created
   // Class name, Lecture list
-  deployer.deploy(Study);
+  deployer.deploy(Lab);
 };
