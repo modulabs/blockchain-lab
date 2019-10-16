3315037c8953e268755a3c040ebafb3b4ea4a6ae
kmh <kimminhyun@comcom.ai>
2019-10-16 20:37:13 +0900

    move season 1

--- client/src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import About from "./About";
-import Home from "./Home";
-import MyStudies from "./MyStudies";
-import Studies from "./Studies";
-
-export { About, Home, MyStudies, Studies };
--- migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-var Lab = artifacts.require("./Lab.sol")
-
-module.exports = function(deployer) {
-  // Class defined with Lectures when created
-  // Class name, Lecture list
-  deployer.deploy(Lab, "Blockchain Lab");
-};
