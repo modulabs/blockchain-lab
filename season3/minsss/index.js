08f2ae1680795b880b263ae57adad410e1c117c0
Minsu Lee <minsulee0326@gmail.com>
2019-10-30 20:47:11 +0900

    Get trading pairs

--- season3/minsss/index.js
+++ season3/minsss/index.js
@@ -1,33 +1,35 @@
-const crypto = require('crypto');
 const request = require('request');
 
-// 발급받은 api키와 시크릿키를 입력한다
-const apikey = '';
-const secret = '';
-
 const method = 'GET';
-const requestPath = '/assets';
+
+const ASSETS = '/assets';
+const TRADING_PAIRS = '/trading-pairs';
 
 const host = 'api.gopax.co.kr';
 
-var options = {
+const assetsOption = {
   method,
   json: true,
-  url: `https://${host}${requestPath}`,
-  /*
-  headers: {   // Not for now.
-    'API-KEY': apikey,
-    Signature: sign,
-    Nonce: nonce,
-  },
-  */
+  url: `https://${host}${ASSETS}`,
   strictSSL: false,
 };
 
-request(options, (err, response, b) => {
-  if (err) {
-    console.log('err:', err);
-    return;
-  }
-  console.log(b);
-});
+const tradingOption = {
+  method,
+  json: true,
+  url: `https://${host}${TRADING_PAIRS}`,
+  strictSSL: false,
+};
+
+function requestWithOption(option) {
+  request(option, (err, response, b) => {
+    if (err) {
+      console.log('err:', err);
+      return;
+    }
+    console.log(b);
+  });
+}
+
+requestWithOption(assetsOption);
+requestWithOption(tradingOption);
